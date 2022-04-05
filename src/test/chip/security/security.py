fcc4a0d56f564c39a2c57958295a5220d485f75e
hailiang.qin <qinhailiang@hotmail.com>
2022-04-04 22:36:52 -0400

    add some scripts

---
+++ src/test/chip/security/security.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+"""for docstring"""
+
+from config   import ChipConfigBase
+from config   import ChipSecurityConfig
+from testbase import TestBase
+
+
+test ChipSecurityBase(TestBase):
+    self.conf = ChipConfigBase
+    self.vcs_run.args.append("ChipSecurityBase.vcs_run.args")
+    #self.skip_check_items.append("UVM_ERROR :    0")
+    #self.skip_check_items.append("UVM_FATAL :    0")
+    self.when.append("security")
+endtest
+
+test ChipSecuritySanity(ChipSecurityBase):
+    self.conf = ChipSecurityConfig
+    self.vcs_run.args.append("ChipSecuritySanity.vcs_run.args")
+    #self.when.append("security")
+endtest
+
+test ChipSecurityRSA(ChipSecurityBase):
+    self.conf = ChipSecurityConfig
+    self.vcs_run.args.append("ChipSecurityRSA.vcs_run.args")
+    #self.when.append("security")
+endtest
+
---
+++ src/verif/soc_top_tb.sv
@@ -0,0 +1,9 @@
+module top_tb;
+    import uvm_pkg::*;
+    initial begin
+        #1ns;
+        `uvm_info("soc_top_tb", "******************************************************************************", UVM_NONE)
+        `uvm_info("soc_top_tb", "**************************in soc_top_tb for default **************************", UVM_NONE)
+        `uvm_info("soc_top_tb", "******************************************************************************", UVM_NONE)
+    end
+endmodule
